f523b3ec48dd897043473785b437997c26220edf
Karol Moroz <karol@moroz.dev>
2026-06-18 19:31:07 +0200

    Add KMS API demo from part 2

--- src/kms.ts
+++ src/kms.ts
@@ -1,4 +1,4 @@
-import { KMSClient } from "@aws-sdk/client-kms";
+import { GenerateDataKeyCommand, KMSClient } from "@aws-sdk/client-kms";
 
 function mustGetenv(name: string): string {
   const value = process.env[name];
@@ -6,9 +6,17 @@ function mustGetenv(name: string): string {
     console.error(`FATAL: Environment variable ${name} is not set!`);
     process.exit(1);
   }
-  return value as string;
+  return value;
 }
 
 const ENCRYPTION_KEY_ID = mustGetenv("ENCRYPTION_KEY_ID");
 
 const kmsClient = new KMSClient();
+
+const generateDataKeyResponse = await kmsClient.send(
+  new GenerateDataKeyCommand({
+    KeyId: ENCRYPTION_KEY_ID,
+    KeySpec: "AES_256",
+  }),
+);
+console.log({ generateDataKeyResponse });
